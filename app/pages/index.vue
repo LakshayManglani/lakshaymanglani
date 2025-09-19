f0d6d754c1f119c0842db0386568346c45d49266
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2025-09-19 20:14:25 +0530

    feat: setup nuxt ui
    - added `@nuxt/ui` module to the `nuxt.config.ts` file
    
    - added dependencies:
      - @nuxt/ui

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <UContainer>
+    <h1 class="text-center text-4xl text-primary mt-4">Lakshay Manglani</h1>
+  </UContainer>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
   vite: {
     plugins: [tailwindcss()],
   },
