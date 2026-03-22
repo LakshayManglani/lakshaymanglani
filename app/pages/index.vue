2c391edee55044735de7638ebfd4abfa3ed565ae
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2026-03-22 13:37:07 +0530

    feat: setup vueuse
    - updated `nuxt.config.ts` with the @vueuse/nuxt module
    - added dev-dependencies
      - @vueuse/core
      - @vueuse/nuxt

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const { x, y } = useMouse();
+</script>
+
 <template>
   <div>
+    <div>pos: {{ x }}, {{ y }}</div>
     <h1 class="text-5xl text-highlighted">Hello World!</h1>
   </div>
 </template>
