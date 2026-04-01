6574f5ed71799580c57daef1539e3a42e4af6b38
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2026-04-01 16:53:36 +0530

    feat: add layout transition

--- app/app.vue
+++ app/app.vue
@@ -27,4 +27,14 @@ provide('navigation', navigation);
   opacity: 0;
   filter: blur(1rem);
 }
+
+.layout-enter-active,
+.layout-leave-active {
+  transition: all 0.4s;
+}
+.layout-enter-from,
+.layout-leave-to {
+  filter: blur(1rem);
+  scale: 0.8;
+}
 </style>
